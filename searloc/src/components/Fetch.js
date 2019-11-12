2e827b2dff67012833ba55cd0e1e35fdc7372cfe
Tasos Kourtis <tasoskour1@gmail.com>
2019-11-12 23:28:04 +0200

    Udate component

--- searloc/src/components/Fetch.js
+++ searloc/src/components/Fetch.js
@@ -19,7 +19,6 @@ this.getKeyword=this.getKeyword.bind(this)
   getKeyword(keyword){
 //this.setState({ keyword: "Athens"})
     var API = 'http://35.180.182.8/Search?keywords='+keyword+'&language=en&limit=5';
-console.log(API)
     fetch(API)
       .then(response => response.json())
       .then(data => this.setState({ entries: data.entries, isLoading: false }));
@@ -28,7 +27,6 @@ console.log(API)
 
 componentDidUpdate(prevProps)  {
 if(prevProps.keyword!==this.props.keyword){
-  console.log("P:"+prevProps.keyword)
   this.getKeyword(prevProps.keyword)
 }
 }
@@ -42,11 +40,6 @@ if(prevProps.keyword!==this.props.keyword){
 
     return (
       <div>
-
-
-
-<h1>{this.state.keyword}</h1>
-
       <ul>
         {entries.map(entry =>
           <li key={entry.name}>
